4baef0133112d9c59f4bc9d7c6b0d22cd7245315
Sushant Sahu <sushantsahu@Sushants-MacBook-Pro.local>
2017-02-01 18:44:37 +0530

    initial commit

--- HelloUITableview/ViewController.swift
+++ HelloUITableview/ViewController.swift
@@ -12,13 +12,70 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        requestProfile()
+        
+        
     }
+    
+    func requestProfile() {
+        
+        let url = URL(string: "http://localhost:3000/people/20")
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config)
+        
+        let task = session.dataTask(with: url!) {
+        
+            (data,response,error) in
+            
+            if(error != nil) {
+                print(error)
+            }else {
+                print("success !!")
+                /*if let str = String(data: data!, encoding: String.Encoding.utf8) {
+                    print(str)
+                }*/
+                
+                do {
+                 
+                    let json = try JSONSerialization.jsonObject(with: data!, options: .mutableContainers)
+                    //print(json)
+                    
+                    for dictionary in json as! [[String:AnyObject]] {
+                        let fname = dictionary["fname"] as! String
+                        let lname = dictionary["lname"] as! String
+                        let address = dictionary["address"] as! String
+                        let image = dictionary["image"] as! String
+                        print("\(fname), \(lname)")
+                        print("Living at \(address)")
+                        print(image)
+                    }
+                    
+                    
+                } catch let jsonError {
+                    print(jsonError)
+                }
+                
+                
+                
+            }
+            
+        }
+        
+        task.resume()
+        
+        
+        
+        
+        
+    }
+    
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
+    
+    
 
 
 }
