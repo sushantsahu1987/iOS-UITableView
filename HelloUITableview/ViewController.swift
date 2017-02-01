5ee77aa29648e48b08b3a67d91c2a5dfc5e974b3
Sushant Sahu <sushantsahu@Sushants-MacBook-Pro.local>
2017-02-01 10:58:30 +0530

    Initial Commit

---
+++ HelloUITableview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloUITableview
+//
+//  Created by Sushant Sahu on 01/02/17.
+//  Copyright © 2017 Sushant Sahu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
