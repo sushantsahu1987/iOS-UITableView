fc0532df9370878dd8a14bef6d31628907d32abd
Sushant Sahu <sushantsahu@Sushants-MacBook-Pro.local>
2017-02-02 14:06:31 +0530

    Added checks to see if internet connection is available or not

--- HelloUITableview/ViewController.swift
+++ HelloUITableview/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import SystemConfiguration
 
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
@@ -15,6 +16,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
         tableProfile.dataSource = self
         tableProfile.delegate = self
+        
+        
+        let isConnected = isConnectedToNetwork();
+        print("is connected to n/w \(isConnected)")
         
         requestProfile()
         
@@ -144,9 +149,32 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         task.resume()
         
         
+    }
+    
+    
+    func isConnectedToNetwork() -> Bool {
         
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(MemoryLayout<sockaddr_in>.size)
+        zeroAddress.sin_family = sa_family_t(AF_INET)
         
+        guard let defaultRouteReachability = withUnsafePointer(to: &zeroAddress, {
+            $0.withMemoryRebound(to: sockaddr.self, capacity: 1) {
+                SCNetworkReachabilityCreateWithAddress(nil, $0)
+            }
+        }) else {
+            return false
+        }
         
+        var flags: SCNetworkReachabilityFlags = []
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability, &flags) {
+            return false
+        }
+        
+        let isReachable = flags.contains(.reachable)
+        let needsConnection = flags.contains(.connectionRequired)
+        
+        return (isReachable && !needsConnection)
     }
     
     
